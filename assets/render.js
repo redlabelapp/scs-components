c4fee3124f3fcf3c2b17ae9b3403c603d8645064
brito <francisco.brito@oracle.com>
2017-03-17 21:25:54 -0700

    Load remote tweets using proxy, fancy ko bindings here.

--- assets/render.js
+++ assets/render.js
@@ -15,21 +15,31 @@ define ([
 
 	function View(args, container){
 		this.screen_name = ko.observable();
+		this.tweets = ko.observableArray();
+
 		ko.applyBindings(this, this.content = $(template).appendTo(container)[0]);
 
 		// bit.ly/Sites-SDK
 		var Sites = args.SitesSDK;
 		Sites.subscribe(Sites.MESSAGE_TYPES.SETTINGS_UPDATED, update(this));
-		
+
 		Sites.getProperty('customSettingsData', update(this));
 	}
 
 	function update(view){
-		/* callback for getProperty or SETTINGS_UPDATED message */
+		/* callback for getProperty or SETTINGS_UPDATED */
 		return function(settings){
 			view.screen_name((settings.value || settings).params.screen_name);
-			// https://dev.twitter.com/web/javascript/events
-			//twttr.widgets.load(view.content);
+			proxyLoad(view);
 		};
+	}
+
+	function proxyLoad(view){
+		var config = JSON.parse(appinfo).initialData.customSettingsData,
+			url = config.proxyUrl + config.basePath + config.apiPath;
+
+		ko.computed(function(){
+			$.getJSON(url, config.params, view.tweets);
+		}, view);
 	}
 });
